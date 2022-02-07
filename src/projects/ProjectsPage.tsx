690ce24456e972c89379c56175065c7d704b917d
Kenz <yutthagarn.i@kbtg.tech>
2022-02-07 08:43:56 +0700

    Lab 6

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {MOCK_PROJECTS} from "./MockProjects";
+import ProjectList from "./  ProjectList";
 
 function ProjectsPage() {
-    return <h1>Projects</h1>;
+    return (
+        <>
+            <h1>Projects</h1>
+            <ProjectList projects={MOCK_PROJECTS}/>
+        </>
+    );
 }
 
 export default ProjectsPage;
