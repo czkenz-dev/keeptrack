b37639777321c92f7fbb8b281a97f9fd0b25cc60
Kenz <yutthagarn.i@kbtg.tech>
2022-02-08 11:28:36 +0700

    Lab 12

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,27 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import {Project} from "./Project";
+import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
     projects: Project[];
 }
 
 function ProjectList({projects}: ProjectListProps) {
-    return (
-        <div className="row">
-            {projects.map((project) => (
-                <div key={project.id} className="cols-sm">
-                    <img src={project.imageUrl} alt={project.name}/>
-                    <section className="section dark">
-                        <h5 className="strong">
-                            <strong>{project.name}</strong>
-                        </h5>
-                        <p>{project.description}</p>
-                        <p>Budget : {project.budget.toLocaleString()}</p>
-                    </section>
-                </div>
-            ))}
+    const [projectBeingEdited, setProjectBeingEdited] = useState({});
+    const handleEdit = (project: Project) => {
+        setProjectBeingEdited(project);
+    };
+    const items = projects.map(project => (
+        <div key={project.id} className="cols-sm">
+            {project === projectBeingEdited ? (
+                <ProjectForm/>
+            ) : (
+                <ProjectCard project={project} onEdit={handleEdit}/>
+            )}
         </div>
-    );
+    ));
+    return <div className="row">{items}</div>;
 }
 
 export default ProjectList;
