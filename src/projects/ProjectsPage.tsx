e921e7745271fabb22b73843fc3852335bda9089
Kenz <yutthagarn.i@kbtg.tech>
2022-02-08 09:23:09 +0700

    Lab 7

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {MOCK_PROJECTS} from "./MockProjects";
-import ProjectList from "./  ProjectList";
+import ProjectList from "./ProjectList";
 
 function ProjectsPage() {
     return (
